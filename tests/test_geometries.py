c4c52b12006ca592037c184d5ff4021d2a7d56bb
David Landa Marban <dmar@norceresearch.no>
2023-09-15 02:55:13 +0200

    Fix to the test fail

--- tests/test_geometries.py
+++ tests/test_geometries.py
@@ -4,34 +4,17 @@
 """Test the different type of grids"""
 
 import os
-import math as mt
 
 
 def test_geometries():
     """See geometries/"""
     cwd = os.getcwd()
     os.chdir(f"{os.getcwd()}/tests/geometries")
-    npr = 5
     conf_fil = sorted([name[:-4] for name in os.listdir(".") if name.endswith(".txt")])
-    nsimulations = len(conf_fil)
-    for i in range(mt.floor(nsimulations / npr)):
-        command = ""
-        for j in range(npr):
-            command += (
-                f"pyopmnearwell -i {conf_fil[npr*i+j]}.txt -o {conf_fil[npr*i+j]} & "
-            )
-        command += "wait"
+    for file in conf_fil:
+        command = f"pyopmnearwell -i {file}.txt -o {file} & wait"
+        print(command)
         os.system(command)
-    finished = npr * mt.floor(nsimulations / npr)
-    remaining = nsimulations - finished
-    command = ""
-    for i in range(remaining):
-        command += (
-            f"pyopmnearwell -i {conf_fil[finished+i]}.txt -o {conf_fil[finished+i]} & "
-        )
-    command += "wait"
-    os.system(command)
-    for i in range(nsimulations):
-        assert os.path.exists(f"./{conf_fil[i]}/postprocessing/saturation_2D.png")
-        os.system(f"rm -rf {conf_fil[i]}")
+        assert os.path.exists(f"./{file}/postprocessing/saturation_2D.png")
+        os.system(f"rm -rf {file}")
     os.chdir(cwd)
